53eb5dfec434e757e5e874b8e01ad60904af4158
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-12 09:35:45 +0000

    BUG: where clause always returning true

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,15 +1,15 @@
 -- Creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN users_id INT)
 BEGIN
   DECLARE avg_score FLOAT;
 
   SELECT AVG(score) INTO avg_score
   FROM corrections
-  WHERE `user_id` = user_id;
+  WHERE `user_id` = users_id;
 
   UPDATE users
   SET average_score = avg_score
-  WHERE id = user_id;
+  WHERE id = users_id;
 END$$
 DELIMITER ;
