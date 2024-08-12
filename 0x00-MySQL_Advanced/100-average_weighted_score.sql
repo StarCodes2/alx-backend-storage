7c8fbe462b52ffb80c7b5216f6ed57ea480298e2
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-12 15:43:38 +0000

    To compute average weighted score for a user

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,27 @@
+-- SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average weighted score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (IN users_id INT)
+BEGIN
+  DECLARE weighted_sum FLOAT DEFAULT 0;
+  DECLARE total_weight FLOAT DEFAULT 0;
+  DECLARE avg_score FLOAT DEFAULT 0;
+
+  SELECT SUM(score * weight), SUM(weight)
+  INTO weighted_sum, total_weight
+  FROM corrections
+  JOIN projects
+    ON corrections.project_id = projects.id
+  WHERE corrections.user_id = users_id;
+
+  IF total_weight > 0 THEN
+    SET avg_score = weighted_sum / total_weight;
+  ELSE
+    SET avg_score = 0;
+  END IF;
+
+  UPDATE users
+  SET average_score = avg_score
+  WHERE id = users_id;
+END$$
+DELIMITER ;
