b1052ba8f980139c14de7427a556133105ed9800
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-14 20:25:38 +0000

    Class Cache

--- 0x02-redis_basic/README.md
+++ 0x02-redis_basic/README.md
@@ -1,2 +1,3 @@
 # 0x02. Redis basic
 ## Python File
+- **exercise.py**: Contains the Cache class that uses Redis to cache data.
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    Defines the Cache class that uses Redis to cache data.
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache():
+    """ Using Redis to cache data. """
+    def __init__(self):
+        """ Init an instance of Cache. """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[bytes, int, str, float]) -> str:
+        key: str = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
