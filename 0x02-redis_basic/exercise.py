92cf11a6d9a217c402e4e3d5eada1d9f9c69bfbf
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-14 21:28:18 +0000

    To return a value as it original data type.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 """
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Optional, Union
 
 
 class Cache():
@@ -18,3 +18,23 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes,
+                                                                    int,
+                                                                    float]:
+        """ Returns a value in a specific data format. """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """ Return a value in string format. """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[str]:
+        """ Return a value in int format. """
+        return sefl.get(key, fn=int)
