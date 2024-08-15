5f6f577b119a41a73004b5f524601393b09563fc
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-15 00:01:22 +0000

    To cache the HTML response of a url request.

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+    Defines a function that uses the requests module to obtain the HTML
+    content of a particular URL and returns it, while using a decorator to
+    cache the result and track the number of times a particular URL was
+    accessed.
+"""
+import requests
+import redis
+from functools import wraps
+from typing import Callable
+
+red = redis.Redis()
+
+def cache_response(func: Callable) -> Callable:
+    """Decorator to cache the result of a function with an expiration time."""
+    @wraps(func)
+    def wrapper(url: str) -> str:
+        ca_result = red.get(url)
+        if ca_result:
+            return ca_result.decode('utf-8')
+        result = func(url)
+        red.setex(url, 10, result)
+
+        return result
+    return wrapper
+
+@cache_response
+def get_page(url: str) -> str:
+    """ Fetch the HTML content of a url and cache it. """
+    red.incr("count:{}".format(url))
+    response = requests.get(url)
+    return response.text
