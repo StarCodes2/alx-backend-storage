b88fcb22352836f18f6127af8c68816d8dc25880
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-16 17:14:59 +0000

    Updates

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -18,8 +18,8 @@ def cache_response(func: Callable) -> Callable:
     """Decorator to cache the result of a function with an expiration time."""
     @wraps(func)
     def wrapper(url: str) -> str:
-        red.incr("count:{}".format(url))
-        red.expire("count:{}".format(url), 10)
+        red.incr("count:{{{}}}".format(url))
+        red.expire("count:{{{}}}".format(url), 10)
         return func(url)
 
     return wrapper
